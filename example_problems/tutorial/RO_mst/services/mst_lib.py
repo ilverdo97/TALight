412f2ce2c327dcdd83101b10229744b887fe2dbc
riccardo <riccardovr97@hotmail.it>
2022-08-23 00:23:54 +0200

    added format check

--- example_problems/tutorial/RO_mst/services/mst_lib.py
+++ example_problems/tutorial/RO_mst/services/mst_lib.py
@@ -1,4 +1,4 @@
-import ast, math, operator, itertools
+import ast, math, operator, itertools, re
 from sys import stderr
 from typing import Optional, List, Dict, Callable
 from dataclasses import dataclass
@@ -52,7 +52,97 @@ class verify_submission_problem_specific(verify_submission_gen):
     def verify_format(self, SEF):
         if not super().verify_format(SEF):
             return False
-        # TODO
+
+        if 'opt_sol' in self.goals:
+            g = self.goals['opt_sol']
+            if type(g.answ) != str:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere una stringa di archi. Una lista di archi è data dagli è "
+                                        f"costituita da una lista di inidici riferiti alla lista degli archi "
+                                        f"nell'sitanza del problema")
+            try:
+                g_eval = ast.literal_eval(g.answ)
+                if type(g_eval) != list:
+                    return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                            f"immettere una lista di archi. Una lista di archi è costituita da una "
+                                            f"lista di inidici riferiti alla lista degli archi "
+                                            f"nell'sitanza del problema")
+                if not all([isinstance(arch, int) for arch in g_eval]):
+                    return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                            f"immettere una lista di archi. Una lista di archi è costituita da una "
+                                            f"lista di inidici riferiti alla lista degli archi "
+                                            f"nell'sitanza del problema")
+            except SyntaxError:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere una lista di archi. Impossibile effettuare il parsing dell'input: "
+                                        f"errore nella sintassi.")
+            SEF.format_OK(g, f"come `{g.alias}` hai immesso una stringa come richiesto",
+                          f"ovviamente durante lo svolgimento dell'esame non posso dirti se la stringa inserita sia "
+                          f"poi la risposta corretta, ma il formato è corretto")
+
+        if 'opt_val' in self.goals:
+            g = self.goals['opt_val']
+            if type(g.answ) != int:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere un intero.")
+            SEF.format_OK(g, f"come `{g.alias}` hai immesso un intero come richiesto",
+                          f"ovviamente durante lo svolgimento dell'esame non posso dirti se l'intero immesso sia poi "
+                          f"la risposta corretta, ma il formato è corretto")
+
+        if 'num_opt_sols' in self.goals:
+            g = self.goals['num_opt_sols']
+            if type(g.answ) != int:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere un intero.")
+            SEF.format_OK(g, f"come `{g.alias}` hai immesso un intero come richiesto",
+                          f"ovviamente durante lo svolgimento dell'esame non posso dirti se l'intero immesso sia poi "
+                          f"la risposta corretta, ma il formato è corretto")
+
+        if 'list_opt_sols' in self.goals:
+            g = self.goals['list_opt_sols']
+            if type(g.answ) != str:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere una stringa di archi. Una lista di archi è data dagli è "
+                                        f"costituita da una lista di inidici riferiti alla lista degli archi "
+                                        f"nell'sitanza del problema")
+            try:
+                g_eval = ast.literal_eval(g.answ)
+                if type(g_eval) != list:
+                    return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                            f"immettere una lista di archi. Una lista di archi è costituita da una "
+                                            f"lista di inidici riferiti alla lista degli archi "
+                                            f"nell'sitanza del problema")
+                if not all([isinstance(graph, list) for graph in g_eval]):
+                    return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                            f"immettere una lista di archi. Una lista di archi è costituita da una "
+                                            f"lista di inidici riferiti alla lista degli archi "
+                                            f"nell'sitanza del problema")
+                for tree in g_eval:
+                    if not all([isinstance(arch, int) for arch in tree]):
+                        return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                                f"immettere una lista di archi. Una lista di archi è costituita da una "
+                                                f"lista di inidici (interi) riferiti alla lista degli archi "
+                                                f"nell'sitanza del problema")
+            except SyntaxError:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere una lista di archi. Impossibile effettuare il parsing dell'input: "
+                                        f"errore nella sintassi.")
+            SEF.format_OK(g, f"come `{g.alias}` hai immesso una stringa come richiesto",
+                          f"ovviamente durante lo svolgimento dell'esame non posso dirti se la stringa inserita sia "
+                          f"poi la risposta corretta, ma il formato è corretto")
+
+        if 'edge_profile' in self.goals:
+            g = self.goals['edge_profile']
+            if type(g.answ) != str:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}' dove era invece richiesto di "
+                                        f"immettere una stringa.")
+            if g.answ not in ['in_all', 'in_no', 'in_some_but_not_in_all']:
+                return SEF.format_NO(g, f"Come '{g.alias}' hai immesso '{g.answ}', Risposte valide: in_all, in_no, "
+                                        f"in_some_but_not_in_all ")
+            SEF.format_OK(g, f"come '{g.alias}' hai immesso una stringa come richiesto",
+                          f"ovviamente durante lo svolgimento dell'esame non posso dirti se la stringa inserita sia "
+                          f"poi la risposta corretta, ma il formato è corretto")
+
 
     def verify_feasibility(self, SEF):
         if not super().verify_feasibility(SEF):
