c112a558cd1295d1b13d5c06e8250aa85fc96765
riccardo <riccardovr97@hotmail.it>
2022-08-24 19:57:34 +0200

    added instance consistency check and optimality check

--- example_problems/tutorial/RO_mst/services/mst_lib.py
+++ example_problems/tutorial/RO_mst/services/mst_lib.py
@@ -6,7 +6,7 @@ from numpy import true_divide
 import networkx as nx
 from RO_verify_submission_gen_prob_lib import verify_submission_gen
 
-instance_object_specs = {
+instance_objects_spec = {
     ('n', int),
     ('s', int),
     ('t', int),
@@ -37,6 +37,69 @@ answer_objects_implemented = [
     # 'edgecut_cert',
     # 'cutshore_cert',
 ]
+
+
+def check_isolated_nodes(n, edges):
+    nodes_founded = set()
+    for u, v, w in edges:
+        nodes_founded.add(u)
+        nodes_founded.add(v)
+    return len(nodes_founded) != n
+
+
+def check_isolated_nodes_by_forbidden(n, edges, forbidden_edges):
+    nodes_founded = set()
+    for i, (u, v, w) in enumerate(edges):
+        if i not in forbidden_edges:
+            nodes_founded.add(u)
+            nodes_founded.add(v)
+    return len(nodes_founded) != n
+
+
+def check_instance_consistency(instance):
+    print(f"instance={instance}", file=stderr)
+    n = instance['n']
+    m = instance['m']
+    edges = instance['edges']
+    forbidden_edges = instance['forbidden_edges']
+    forced_edges = instance['forced_edges']
+    query_edge = instance['query_edge']
+
+    n = int(n)
+    m = int(m)
+    edges = ast.literal_eval(re.sub(r"[{}]", "", edges))
+    forbidden_edges = ast.literal_eval(forbidden_edges)
+    forced_edges = ast.literal_eval(forced_edges)
+    query_edge = int(query_edge)
+
+    if m != len(edges):
+        print(f"Errore: il numero di archi non corrisponde con la lista data")
+        exit(0)
+    if not all(w >= 0 for _, _, w in edges):
+        print(f"Errore: alcuni pesi sono minori di 0")
+    if not all(0 < u < n and 0 < u < n for u, v, _ in edges):
+        print(f"Errore: alcuni nodi dati negli archi non esistono")
+        exit(0)
+    if not all(u != v for u, v, _ in edges):
+        print(f"Errore: il grafo non può contenere autoloop")
+        exit(0)
+    if not all(0 < e < m for e in forbidden_edges):
+        print(f"Errore: alcuni archi dichiarati in forbidden_edges non esistono")
+        exit(0)
+    if not all(0 < e < m for e in forced_edges):
+        print(f"Errore: alcuni archi dichiarati in forced_edges non esistono")
+        exit(0)
+    if query_edge < 0 or query_edge >= m:
+        print(f"Errore: il query_edge non esiste")
+        exit(0)
+    if query_edge in forbidden_edges:
+        print(f"Errore: il query_edge è nei forbidden_edges")
+    if check_isolated_nodes(n, edges):
+        print(f"Errore: sono presenti dei nodi isolati")
+        exit(0)
+    if check_isolated_nodes_by_forbidden(n, edges, forbidden_edges):
+        print(f"Errore: sono presenti dei nodi isolati dovuti ad archi eliminati da forbidden_edges")
+        exit(0)
 
 
 class Graph:
@@ -98,8 +161,8 @@ def solver(input_to_oracle):
     m = instance['m']
     edges = instance['edges']
     forbidden_edges = instance['forbidden_edges']
-    forced_edges = instance['edges_edges']
-    query_edges = instance['query_edges']
+    forced_edges = instance['forced_edges']
+    query_edges = instance['query_edge']
 
     edges = ast.literal_eval(edges)
     forbidden_edges = ast.literal_eval(forbidden_edges)
@@ -110,6 +173,15 @@ def solver(input_to_oracle):
         graph.add_edge(u, v, e[1], l)
 
     opt_sol, opt_val = graph.kruskalFR(forced_edges, forbidden_edges)
+
+    print(f"input_to_oracle={input_to_oracle}", file=stderr)
+    input_data = input_to_oracle["input_data_assigned"]
+    print(f"Instance={input_data}", file=stderr)
+    oracle_answers = {}
+    for std_name, ad_hoc_name in input_to_oracle["request"].items():
+        oracle_answers[ad_hoc_name] = locals()[std_name]
+    print(oracle_answers)
+    return oracle_answers
 
 
 class verify_submission_problem_specific(verify_submission_gen):
@@ -209,6 +281,7 @@ class verify_submission_problem_specific(verify_submission_gen):
             SEF.format_OK(g, f"come '{g.alias}' hai immesso una stringa come richiesto",
                           f"ovviamente durante lo svolgimento dell'esame non posso dirti se la stringa inserita sia "
                           f"poi la risposta corretta, ma il formato è corretto")
+        return True
 
 
     def verify_feasibility(self, SEF):
@@ -224,4 +297,22 @@ class verify_submission_problem_specific(verify_submission_gen):
     def verify_optimality(self, SEF):
         if not super().verify_optimality(SEF):
             return False
-        # TODO
+        true_opt_val = SEF.oracle_dict['opt_val']
+        true_opt_sol = SEF.oracle_dict['opt_sol']
+        if 'opt_val' in self.goals:
+            g_val = self.goals['opt_val']
+            if true_opt_val != g_val.answ:
+                return SEF.optimality_NO(g_val, f"Il valore ottimo corretto è {true_opt_val} {'>' if true_opt_val != g_val.answ else '<'} {g_val.answ}, che è il valore invece immesso in `{g_val.alias}`. Una soluzione di valore ottimo è {true_opt_sol}.")
+            else:
+                SEF.optimality_OK(g_val, f"{g_val.alias}={g_val.answ} è effettivamente il valore ottimo.", "")
+        if 'opt_sol' in self.goals:
+            g_sol = self.goals['opt_sol']
+            g_sol_answ = self.goals['opt_sol'].answ
+            g_val_answ = sum(
+                [val for ele, cost, val in zip(self.I.labels, self.I.costs, self.I.vals) if ele in g_sol_answ])
+            assert g_val_answ <= true_opt_val
+            if g_val_answ < true_opt_val:
+                return SEF.optimality_NO(g_sol, f"Il valore totale della soluzione immessa in `{g_sol.alias}` è {g_val_answ} < {true_opt_val}, valore corretto per una soluzione ottima quale {true_opt_sol}. La soluzione (ammissibile) che hai immesso è `{g_sol.alias}`={g_sol.answ}.")
+            else:
+                SEF.optimality_OK(g_sol, f"Confermo l'ottimailtà della soluzione {g_sol.alias}={g_sol.answ}.", "")
+        return True
